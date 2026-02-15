a5b5fbfe0d51cb66aaf060dc1e0fcb4e7873a87a
Ben Davies <bdavies1993@gmail.com>
2026-02-15 22:28:40 +0000

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 21
+        versionCode = 1
         versionName = flutter.versionName
     }
 
